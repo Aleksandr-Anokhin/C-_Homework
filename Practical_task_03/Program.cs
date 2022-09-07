2469f51dbcaf7499d0594ccafd1e4f8f5aacc495
Aleksandr-Anokhin <aleksandr0788@gmail.com>
2022-09-07 15:47:29 +0300

    Решил задачу 23

--- Practical_task_03/Program.cs
+++ Practical_task_03/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 19
 // Напишите программу, которая принимает на вход пятизначное число и проверяет, 
 // является ли оно палиндромом.
-/*
+
 Console.WriteLine("Введите пятизначное число. Программа проверит, является ли оно палиндромом.");
 string numPal = Console.ReadLine();
 if (numPal[0]==numPal[4] && numPal[1]==numPal[3])
@@ -9,7 +9,7 @@ if (numPal[0]==numPal[4] && numPal[1]==numPal[3])
     Console.WriteLine("Это число является палиндромом.");
 }
 else Console.WriteLine("Число Не является палиндромом.");
-*/
+
 
 // Задача 21
 // Напишите программу, которая принимает на вход координаты двух точек и находит 
@@ -38,3 +38,15 @@ int z2 = Convert.ToInt32(Console.ReadLine());
 double P = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
 Console.WriteLine($"Расстояние между точками: {Math.Round(P,2)}");
 
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число N");
+int start = 1;
+int end = Convert.ToInt32(Console.ReadLine());
+while (start <= end)
+{
+    Console.WriteLine(start * start * start);
+     start++;
+}
