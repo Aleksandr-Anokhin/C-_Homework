d68765b6a14b56680bdba264aad6d8dadfee3c95
Aleksandr-Anokhin <aleksandr0788@gmail.com>
2022-09-11 15:48:56 +0300

    Решил задачу 27 и 25 не полностью

---
+++ Practical_task_04/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// Например:
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+/*
+Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+for (i = 1; i < B; i++)
+
+Console.WriteLine($"{A}^{B} = {Math.Pow(A,B)}");
+*/
+
+// Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+/*
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр числа равна: {GetSumNumbers(N)}");
+int GetSumNumbers(int N)
+{
+    int sum = 0;
+    while(N > 0)
+    {
+        sum += N%10;
+        N /= 10;
+    }  
+    return sum; 
+}
+*/
+
+//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и 
+// выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
