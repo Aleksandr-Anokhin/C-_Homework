432fd12e7d62275024b45784cf7fe521b0e66517
Aleksandr-Anokhin <aleksandr0788@gmail.com>
2022-09-17 20:28:43 +0300

    сделал 41-ю задачу

---
+++ Practical_task_06/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Вводите положительные и отрицательные целые числа:   ");
+int[] n = Console.ReadLine().Split(' ').Select(e => Convert.ToInt32(e)).ToArray();
+int count = 0;
+
+for (int i = 0; i < n.Length; i++)
+{
+    if (n[i] > 0) count++;
+}
+
+Console.WriteLine($"Количество положительных чисел: {count}");
