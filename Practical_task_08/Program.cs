747de40f26f0944595a312fbdb533eafaf2866cd
Aleksandr-Anokhin <aleksandr0788@gmail.com>
2022-09-28 23:03:16 +0300

    56 с ошибками

--- Practical_task_08/Program.cs
+++ Practical_task_08/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 54: Задайте двумерный массив. Напишите программу,
 // которая упорядочит по убыванию элементы каждой строки двумерного массива.
-
+/*
 int[,] GetMatrix(int m, int n)
 {
     int[,] matrix = new int[4, 4];
@@ -38,3 +38,48 @@ int[,] ChangeArray(int[,] inputArray)
     return inputArray;
 }
 PrintMatrix(ChangeArray(resultMatrix));
+*/
+
+// Задача 56: Задайте прямоугольный двумерный массив.
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов. 
+
+int sum_str = 0;
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[4, 4];
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < n; j++) 
+        {
+            matrix[i,j] = new Random().Next(11); 
+            
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++) 
+    {
+            
+        for (int m = 0; m < inputMatrix.GetLength(1); m++) 
+        {
+            Console.Write(inputMatrix[i,m] + "\t"); 
+            sum_str += inputMatrix[i,m];
+        }
+        Console.WriteLine("Сумма строки  " + (i + 1) + "  " + sum_str);
+        Console.WriteLine();
+    }
+}
+int[,] resultMatrix = GetMatrix(4, 4); 
+PrintMatrix(resultMatrix);
+
+int ChangeArray(int[,] inputArray)
+{
+    int min_sum = sum_str;
+        if (sum_str < min_sum) min_sum = sum_str; 
+        Console.WriteLine("Минимальная сумма строк равна: " + min_sum);
+        return inputArray;
+}    
+PrintMatrix(ChangeArray(inputArray));
+  
